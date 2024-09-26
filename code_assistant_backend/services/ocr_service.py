6635be854bf447e0a46d2c2987858a144f1ce06f
IMRANDIL <aliimranadil2@gmail.com>
2024-09-26 08:59:09 +0530

    added the sample infrerence api use case with hugging_face api model

--- code_assistant_backend/services/ocr_service.py
+++ code_assistant_backend/services/ocr_service.py
@@ -4,3 +4,19 @@ def extract_text_from_image(image_path):
     reader = easyocr.Reader(['en'])
     result = reader.readtext(image_path, detail=0)
     return " ".join(result)
+
+
+
+
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/microsoft/trocr-base-handwritten"
+headers = {"Authorization": "Bearer hf_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"}
+
+def query(filename):
+    with open(filename, "rb") as f:
+        data = f.read()
+    response = requests.post(API_URL, headers=headers, data=data)
+    return response.json()
+
+output = query("cats.jpg")
